e49ff83489b3ce3e6f1bf50f5aed8d064b63a585
jiswidrik <jiswidrik@gmail.con>
2015-02-08 18:16:44 +0100

    minimum length
    minimum length for title, name and text.

--- i_like/app/models/article.rb
+++ i_like/app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ActiveRecord::Base
+  validates :title, presence: true,
+                    length: { minimum: 2 }
+                    
+  validates :name, presence: true,
+                    length: { minimum: 2 }
+                    
+  validates :text, presence: true,
+                    length: { minimum: 2 }
 end

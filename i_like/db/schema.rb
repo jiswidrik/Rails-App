3743274bdf1a91cf01c4ac8502f245088b564067
jiswidrik <jiswidrik@gmail.con>
2015-02-08 17:53:42 +0100

    added text box for name
    changed everything to add an text box for names.

--- i_like/db/schema.rb
+++ i_like/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204153834) do
+ActiveRecord::Schema.define(version: 20150208163317) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.string   "name"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "test", id: false, force: true do |t|
     t.integer "ID",        limit: 1,        null: false
